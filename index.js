47d79e64340368ae4ca570fb611061c48d1b3710
Ratthasart Rojjanai <feevfeev2@gmail.com>
2026-03-23 21:26:56 +0700

    up

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const multer = require("multer");
 const pool = require("./DB");
 const PORT = 3000;
 
+//images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = file.originalname.substring(file.originalname.lastIndexOf("."));
+    cb(null, `student-${uniqueSuffix}${ext}`);
+  },
+});
+const upload = multer({ storage });
+
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/images", express.static("images")); 
@@ -34,6 +47,21 @@ app.get("/api/test_class", async (req, res) => {
   }
 });
 
+app.get("/api/test_/:id", async (req, res) => {
+  const studentId = req.params.id;
+  try {
+    const sql = `SELECT student.id, student.firstname, student.lastname, student.age, student.class_id, student.image_url FROM student WHERE id = ?`;
+    const [student] = await pool.query(sql, [studentId]);
+    if (student.length > 0) {
+      res.json(student[0]);
+    } else {
+      res.status(404).json({ error: "Student not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.delete("/api/test_/:id", async (req, res) => {
   const studentId = req.params.id;
   try {
@@ -45,18 +73,26 @@ app.delete("/api/test_/:id", async (req, res) => {
   }
 });
 
-//images
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const ext = file.originalname.substring(file.originalname.lastIndexOf("."));
-    cb(null, `student-${uniqueSuffix}${ext}`);
-  },
+app.put("/api/test_/:id", upload.single("image"), async (req, res) => {
+  const studentId = req.params.id;
+  try {
+    const { firstname, lastname, age, class_id } = req.body;
+    let image_url = null;
+
+    if (req.file && req.file.filename) {
+      image_url = `/images/${req.file.filename}`;
+      const sql = `UPDATE student SET firstname = ?, lastname = ?, age = ?, class_id = ?, image_url = ? WHERE id = ?`;
+      await pool.query(sql, [firstname, lastname, age, class_id, image_url, studentId]);
+    } else {
+      const sql = `UPDATE student SET firstname = ?, lastname = ?, age = ?, class_id = ? WHERE id = ?`;
+      await pool.query(sql, [firstname, lastname, age, class_id, studentId]);
+    }
+    res.json({ message: "บันทึกข้อมูลสำเร็จ" });
+  } catch (err) {
+    console.error("Update error", err);
+    res.status(500).json({ error: err.message });
+  }
 });
-const upload = multer({ storage });
 
 app.post("/api/test_", upload.single("image"), async (req, res) => {
   try {
